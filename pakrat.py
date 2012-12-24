a7315930b5b25cdb395edb17066555aec284c88c
Toru Hisai <toru-github@torus.jp>
2012-12-24 13:00:21 +0900

    added some assertions

--- pakrat.py
+++ pakrat.py
@@ -2,12 +2,13 @@
 
 import os
 import hashlib
+import sys
 
 def walk(dir, datdir):
    for root, dirs, files in os.walk(dir):
       print "Current directory", root
-      print "Sub directories", dirs
-      print "Files", files
+      # print "Sub directories", dirs
+      # print "Files", files
 
       for file in files:
          path = os.path.join(root, file)
@@ -23,18 +24,32 @@ def walk(dir, datdir):
          f = os.path.join(d, hash)
          if os.path.exists(f):
             os.unlink(path)
+            print "file duplicated", path, hash
          else:
             os.rename(path, f)
 
          os.link(f, path)
 
 def hashfile(filepath):
-    sha1 = hashlib.sha1()
-    f = open(filepath, 'rb')
-    try:
-        sha1.update(f.read())
-    finally:
-        f.close()
-    return sha1.hexdigest()
+   sha1 = hashlib.sha1()
+   f = open(filepath, 'rb')
+   try:
+      sha1.update(f.read())
+   finally:
+      f.close()
+   return sha1.hexdigest()
 
-walk('./test/dir', './test/dat')
+def main():
+   print sys.argv
+   srcdir, datadir = [os.path.realpath(x) for x in sys.argv[1:3]]
+   print srcdir, datadir
+   assert os.path.isdir(srcdir), "directory not existing: " + srcdir
+   if os.path.exists(datadir):
+      assert os.path.isdir(datadir), "specified path is not a directory: " + datadir
+      assert not os.path.samefile(srcdir, datadir), "specified directories identical"
+
+   comm = os.path.commonprefix([srcdir, datadir])
+   assert comm != srcdir and comm != datadir, "directories inclusive"
+   walk(srcdir, datadir)
+
+main()
